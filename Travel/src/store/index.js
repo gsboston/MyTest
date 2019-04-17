2fe71b9d217dde6a6b5294bb021fbe988ed91809
gsboston <gsboston@163.com>
2019-04-17 21:38:45 +0800

    travel项目修改

--- Travel/src/store/index.js
+++ Travel/src/store/index.js
@@ -10,7 +10,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state,
   mutations,
-  // 使用场景，类似计算属性
+  // 使用场景，二次处理数据，类似计算属性
   getters: {
     doubleCity(state) {
       return state.city + 'hot'
