79c3b3e54808d3569634c9ff7a66219d7d1c7a12
gsboston <gsboston@163.com>
2019-05-29 00:13:22 +0800

    0528

--- nodeCode/nodeTest07_KOA/server0_koa2中间件.js
+++ nodeCode/nodeTest07_KOA/server0_koa2中间件.js
@@ -1,11 +1,7 @@
-function compose(middleware) {
-    if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
-    for (const fn of middleware) {
-        if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
-    }
+import { callbackify } from "util";
 
+function compose(middleware) {
     return function (context, next) {
-        // last called middleware #
         let index = -1
         return dispatch(0)
         function dispatch(i) {
@@ -23,22 +19,49 @@ function compose(middleware) {
     }
 }
 
+let arr = [
+    function (ctx, next) { console.log(1); next(); console.log(2); },
+    function (ctx, next) { console.log(3); }
+];
 
-function compose(context, next) {
+let fn = compose(arr);
+
+fn = function (context, next) {
     let index = -1
     return dispatch(0)
     function dispatch(i) {
-        if (i <= index) return Promise.reject(new Error('next() called multiple times'))
         index = i
-        let fn = middleware[i]
-        if (i === middleware.length) fn = next
-        if (!fn) return Promise.resolve()
+        let midFn = middleware[i]
+        if (i === middleware.length) midFn = next
+        if (!midFn) return Promise.resolve()
         try {
-            return Promise.resolve(fn(context, function next() {
-                return dispatch(i + 1)
-            }))
+            return Promise.resolve(midFn(context, dispatch.bind(null, i + 1)));
         } catch (err) {
             return Promise.reject(err)
         }
     }
 }
+
+function callback(){
+    return fn(ctx).then(handleResponse).catch(onerror);
+}
+
+// function compose(context, next) {
+//     let index = -1
+//     return dispatch(0)
+//     function dispatch(i) {
+//         if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+//         index = i
+//         let fn = middleware[i]
+//         if (i === middleware.length) fn = next
+//         if (!fn) return Promise.resolve()
+//         try {
+//             return Promise.resolve(fn(context, function next() {
+//                 return dispatch(i + 1)
+//             }))
+//         } catch (err) {
+//             return Promise.reject(err)
+//         }
+//     }
+// }
+
