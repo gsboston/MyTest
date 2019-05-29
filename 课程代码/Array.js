9652a15033830d3213b56f501530745ab603c743
gsboston <gsboston@163.com>
2019-05-29 18:15:24 +0800

    0529 array.js

---
+++ 课程代码/Array.js
@@ -0,0 +1,28 @@
+// Array.of
+// Array.from
+// Array.copyWithin
+// Array.fill
+// es7 includes
+// delete 
+
+// 遍历
+// ES5：
+// forEach、// every 、some、 fliter、map、reduce、reduceRight、
+// ES6：
+// find、findIndex、keys、values、entries
+
+// 不改变原数组的方法(8个):
+// ES5：
+// join、toLocateString、toString、slice、concat、indexOf、lastIndexOf、
+// ES7：
+// includes
+
+// 改变原数组的方法(9个):
+// shift/unshift pop push reverse sort splice
+
+// 18. 浅拷贝：Array类型是一种引用类型；当数组a复制给数组b时，对数组b进行元素修改，数组a也会发生修改。
+// 19. 深拷贝：使用concat()方法，返回新的数组；防止浅度复制的情况发生，对数组b进行元素修改操作，数组a不发生变更。
+// 20. 判断2个数组是否相等 Array数组为引用类型，[]===[] 都会返回false，所以可通过数组toString()方法返回的字符串判断是否相等。
+
+// https://www.jb51.net/article/141330.htm
+// https://www.cnblogs.com/lzm1989/p/5967815.html
