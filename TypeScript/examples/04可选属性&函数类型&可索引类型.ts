73252f0419380ead50773d558d28f4f80ba12d35
gsboston <gsboston@163.com>
2019-08-29 08:03:29 +0800

    0829 morning 修改

--- TypeScript/examples/04可选属性&函数类型&可索引类型.ts
+++ TypeScript/examples/04可选属性&函数类型&可索引类型.ts
@@ -89,8 +89,7 @@ interface SearchFunc {
 // 这样定义后，我们可以像使用其它接口一样使用这个函数类型的接口。 
 // 下例展示了如何创建一个函数类型的变量，并将一个同类型的函数赋值给这个变量。
 
-let mySearch: SearchFunc
-mySearch = function (src: string, sub: string): boolean {
+let mySearch: SearchFunc = function (src: string, sub: string): boolean {
     let result = src.search(sub)
     return result > -1
 }
